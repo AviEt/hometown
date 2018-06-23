baaf1f1a2d14028ab9da45971613c159c3d70fb5
aviet <avi.etzioni@gmail.com>
2018-06-23 14:02:17 +0300

    Added an image carousel to the item page if morePhotos is provided

--- src/features/item/item-page.js
+++ src/features/item/item-page.js
@@ -10,12 +10,34 @@ import {
     ItemPhoto,
     ScrollingContent
 } from 'shared/components';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
 
 const styles = theme => ({
     item: {
         display: 'flex',
         flexDirection: 'row',
         padding: theme.spacing.unit * 2
+    },
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+        overflow: 'hidden',
+        backgroundColor: theme.palette.background.paper
+    },
+    gridList: {
+        flexWrap: 'nowrap',
+        // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+        transform: 'translateZ(0)'
+    },
+    title: {
+        color: theme.palette.primary.light
+    },
+    titleBar: {
+        background:
+            'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)'
     }
 });
 
@@ -39,9 +61,34 @@ class ItemPageBase extends React.Component {
         );
     }
 
+    presentImages(classes, morePhotos) {
+        return (
+            <div className={classes.root}>
+                <GridList className={classes.gridList} cols={2.5}>
+                    {morePhotos.map(tile => (
+                        <GridListTile key={tile}>
+                            <img src={tile} alt={'image title'} />
+                            <GridListTileBar
+                                classes={{
+                                    root: classes.titleBar,
+                                    title: classes.title
+                                }}
+                                // actionIcon={
+                                //     <IconButton>
+                                //         <StarBorderIcon className={classes.title} />
+                                //     </IconButton>
+                                // }
+                            />
+                        </GridListTile>
+                    ))}
+                </GridList>
+            </div>
+        );
+    }
+
     presentItem(classes, item, rootStore) {
         if (item.morePhotos) {
-            return <div>Hello World</div>;
+            return this.presentImages(classes, item.morePhotos);
         } else {
             return (
                 <div className={classes.item}>
