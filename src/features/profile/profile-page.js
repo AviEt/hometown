4208c771d5de03e116e018233e9fafc89e7fad46
aviet <avi.etzioni@gmail.com>
2018-06-25 13:21:27 +0300

    Added profile page data

--- src/features/profile/profile-page.js
+++ src/features/profile/profile-page.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import { inject } from 'mobx-react';
 import { HeaderLayout, ScrollingContent } from 'shared/components';
+import { ProfileCard } from 'shared/components';
 
 @inject('rootStore')
 export class ProfilePage extends React.Component {
@@ -13,10 +14,22 @@ export class ProfilePage extends React.Component {
             user: { email }
         } = authStore;
 
+        const profile = {
+            name: 'Avi Etzioni',
+            avatar:
+                'https://scontent.ftlv2-1.fna.fbcdn.net/v/t31.0-8/25542336_10156035104193336_5168887463173703747_o.jpg?_nc_cat=0&oh=39f00dc9bcede1107035a1db7c355f6e&oe=5BAA2AAA',
+            cover:
+                'https://scontent.ftlv2-1.fna.fbcdn.net/v/t1.0-9/18268677_10155349070432853_4734958941778630218_n.jpg?_nc_cat=0&oh=3e71b2bbd31e4ddd7ce86106583e8f70&oe=5BB65107',
+            joined: 'May 06 2017',
+            description:
+                "Avi loves a clean house, especially when he's not the one cleaning. Same goes for good" +
+                ' food. '
+        };
+
         return (
             <HeaderLayout>
                 <ScrollingContent>
-                    <Typography variant="headline">{email}</Typography>
+                    <ProfileCard profile={profile} />
                 </ScrollingContent>
             </HeaderLayout>
         );
